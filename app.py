1213364d7c664c3c039d226e6fc8856ff64a66b6
mazdiam <mazdiam1@gmail.com>
2026-08-28 01:36:24 +0330

    fix website serving

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "سلام"
+    return send_from_directory(".", "index.html")
+
+@app.route("/<path:filename>")
+def files(filename):
+    return send_from_directory(".", filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
